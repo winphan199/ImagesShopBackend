9228f340b3dc55e3392ee5e86fbe0ad2ace3acb5
Minh P. Dang <cunminh1405@gmail.com>
2022-12-12 20:34:41 +0200

    Change to lombok.data

--- src/main/java/com/fullstack2022/payload/request/LoginRequest.java
+++ src/main/java/com/fullstack2022/payload/request/LoginRequest.java
@@ -2,26 +2,13 @@ package com.fullstack2022.payload.request;
 
 import javax.validation.constraints.NotBlank;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
 	@NotBlank
-  private String username;
+  	private String username;
 
 	@NotBlank
 	private String password;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 }
