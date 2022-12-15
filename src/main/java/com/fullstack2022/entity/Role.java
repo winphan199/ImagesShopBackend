7abe5f9983c19c02ccbca2285e86ff73906ffeb9
Minh P. Dang <cunminh1405@gmail.com>
2022-12-15 23:25:34 +0200

    Clean Prj

--- src/main/java/com/fullstack2022/entity/Role.java
+++ src/main/java/com/fullstack2022/entity/Role.java
@@ -2,38 +2,23 @@ package com.fullstack2022.entity;
 
 import javax.persistence.*;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 @Entity
 @Table(name = "roles")
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Role {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
+	@NonNull
 	@Enumerated(EnumType.STRING)
 	@Column(length = 20)
 	private ERole name;
-
-	public Role() {
-
-	}
-
-	public Role(ERole name) {
-		this.name = name;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public ERole getName() {
-		return name;
-	}
-
-	public void setName(ERole name) {
-		this.name = name;
-	}
 }
