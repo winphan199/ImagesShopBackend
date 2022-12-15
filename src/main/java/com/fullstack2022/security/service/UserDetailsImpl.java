17f5ed7c7a154621b8f09aee5b0d86ff1ecbd332
Minh P. Dang <cunminh1405@gmail.com>
2022-12-16 01:04:34 +0200

    Clean Prj

--- src/main/java/com/fullstack2022/security/service/UserDetailsImpl.java
+++ src/main/java/com/fullstack2022/security/service/UserDetailsImpl.java
@@ -12,35 +12,26 @@ import org.springframework.security.core.userdetails.UserDetails;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fullstack2022.entity.User;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class UserDetailsImpl implements UserDetails {
 	private static final long serialVersionUID = 1L;
 
-	private Long id;
+	@Getter private final Long id;
 
-	private String username;
+	@Getter private final String username;
 
-	private String firstName;
+	@Getter private final String firstName;
 
-	private String lastName;
+	@Getter private final String lastName;
 
-	private String email;
+	@Getter private final String email;
 
-	@JsonIgnore
-	private String password;
+	@Getter @JsonIgnore private final String password;
 
-	private Collection<? extends GrantedAuthority> authorities;
-
-	public UserDetailsImpl(Long id, String username, String firstName,  String lastName, String email, String password,
-			Collection<? extends GrantedAuthority> authorities) {
-		this.id = id;
-		this.username = username;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.password = password;
-		this.authorities = authorities;
-	}
+	@Getter private final Collection<? extends GrantedAuthority> authorities;
 
 	public static UserDetailsImpl build(User user) {
 		List<GrantedAuthority> authorities = user.getRoles().stream()
@@ -55,38 +46,6 @@ public class UserDetailsImpl implements UserDetails {
 				user.getEmail(),
 				user.getPassword(), 
 				authorities);
-	}
-
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return authorities;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-
-	@Override
-	public String getPassword() {
-		return password;
-	}
-
-	@Override
-	public String getUsername() {
-		return username;
 	}
 
 	@Override
